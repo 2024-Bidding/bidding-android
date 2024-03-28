2b804a6457e0a1ad71eb0b4bc2aca84babc56f45
정승훈 <hks026naver@gmail.com>
2024-03-28 21:30:51 +0900

    feat :: navigation graph 설정

--- app/src/main/java/com/seunghoon/bidding_android/MainActivity.kt
+++ app/src/main/java/com/seunghoon/bidding_android/MainActivity.kt
@@ -2,7 +2,11 @@ package com.seunghoon.bidding_android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.createGraph
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.fragment
 import com.seunghoon.bidding_android.databinding.ActivityMainBinding
+import com.seunghoon.bidding_android.feature.splash.SplashFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +17,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view_main)
+        val navController = navHostFragment?.findNavController()
+
+        navController?.createGraph(startDestination = NavigationRoute.Auth.SPLASH) {
+            fragment<SplashFragment>(route = NavigationRoute.Auth.SPLASH) {
+                label = NavigationRoute.Auth.SPLASH
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/seunghoon/bidding_android/NavigationRoute.kt
@@ -0,0 +1,11 @@
+package com.seunghoon.bidding_android
+
+sealed class NavigationRoute(
+    val route: String,
+) {
+    data object Auth: NavigationRoute(route = "auth") {
+        val SPLASH = "$route/splash"
+        val SIGN_IN = "$route/signIn"
+        val SIGN_UP = "$route/signUp"
+    }
+}
