8063fdb115b3c7032292ff5e2b99b59aaded2f7e
정승훈 <hks026naver@gmail.com>
2024-04-01 10:45:39 +0900

    feat :: koin di 설정

---
+++ app/src/main/java/com/seunghoon/bidding_android/BiddingApplication.kt
@@ -0,0 +1,36 @@
+package com.seunghoon.bidding_android
+
+import android.app.Application
+import com.seunghoon.bidding_android.data.api.UserApi
+import com.seunghoon.bidding_android.feature.signin.SignInViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+
+class BiddingApplication : Application() {
+
+    private val apiModule = module {
+        single { UserApi() }
+    }
+
+    private val viewModelModule = module {
+        viewModel { SignInViewModel(userApi = get()) }
+    }
+
+    private val biddingModule = module {
+        includes(
+            apiModule,
+            viewModelModule
+        )
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            modules(biddingModule)
+        }
+    }
+}
+
+
