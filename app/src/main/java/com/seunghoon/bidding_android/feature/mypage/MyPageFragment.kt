147557669afb080573a258b9e2cefc3266954311
Tmdhoon2 <hks026naver@gmail.com>
2024-06-24 12:17:21 +0900

    feat :: 로그아웃

--- app/src/main/java/com/seunghoon/bidding_android/feature/mypage/MyPageFragment.kt
+++ app/src/main/java/com/seunghoon/bidding_android/feature/mypage/MyPageFragment.kt
@@ -1,13 +1,23 @@
 package com.seunghoon.bidding_android.feature.mypage
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar.LayoutParams
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.seunghoon.bidding_android.R
 import com.seunghoon.bidding_android.common.insertIntoGlide
+import com.seunghoon.bidding_android.common.showToast
 import com.seunghoon.bidding_android.databinding.FragmentMyPageBinding
 import com.seunghoon.bidding_android.domain.entity.items.ItemsEntity
+import com.seunghoon.bidding_android.navigation.navigateToSignIn
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MyPageFragment : Fragment() {
@@ -30,18 +40,50 @@ class MyPageFragment : Fragment() {
         MyItemsAdapter(items = myBidItems)
     }
 
+    private val navController by lazy {
+        requireActivity().findNavController(R.id.container_main)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         collectMyPageSideEffect()
+        initView()
         with(viewModel) {
             fetchUserInformation()
             fetchMyBidItems()
             fetchMyItems()
         }
         return binding.root
+    }
+
+    private fun initView() {
+        setLogoutButton()
+    }
+
+    private fun setLogoutButton() {
+        binding.btnMyPageLogout.setOnClickListener {
+            val dialog = Dialog(requireContext()).apply {
+                setContentView(R.layout.dialog_logout)
+                window?.setLayout(
+                    LayoutParams.MATCH_PARENT,
+                    LayoutParams.WRAP_CONTENT,
+                )
+
+                findViewById<TextView>(R.id.tv_logout_yes).setOnClickListener {
+                    navController.navigateToSignIn()
+                    this@MyPageFragment.requireContext().showToast("로그아웃 되었습니다!")
+                    hide()
+                }
+
+                findViewById<TextView>(R.id.tv_logout_no).setOnClickListener {
+                    hide()
+                }
+            }
+            dialog.show()
+        }
     }
 
     private fun collectMyPageSideEffect() {
